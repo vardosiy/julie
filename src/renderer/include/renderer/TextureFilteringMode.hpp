2c53d8931fb2140df6daafabd1d845941d6642d5
Vladislav Smirnov <vladislav.smirnov@gameloft.com>
2020-02-19 20:28:41 +0200

    ENUUUUMS

--- src/renderer/include/renderer/TextureFilteringMode.hpp
+++ src/renderer/include/renderer/TextureFilteringMode.hpp
@@ -3,45 +3,10 @@
 #include "utils/Assert.hpp"
 #include "utils/EnumUtils.hpp"
 
-//-----------------------------------------------------------------------------
-
-namespace jl {
-
-//-----------------------------------------------------------------------------
-
-enum class TextureFilteringMode
-{
+NAMESPACE_ENUM_WITH_UTILITY(
+	jl,
+	TextureFilteringMode,
 	Nearest,
 	Linear,
-	LinearMipmapLinear,
-
-	Count
-};
-
-//-----------------------------------------------------------------------------
-
-constexpr std::string_view toString(TextureFilteringMode _tiling)
-{
-	constexpr int lastVerNodeKindsCount = 3;
-	static_assert(static_cast<int>(TextureFilteringMode::Count) == lastVerNodeKindsCount);
-
-	switch (_tiling)
-	{
-		ENUM_TO_STRING_CASE(TextureFilteringMode, Nearest);
-		ENUM_TO_STRING_CASE(TextureFilteringMode, Linear);
-		ENUM_TO_STRING_CASE(TextureFilteringMode, LinearMipmapLinear);
-	}
-
-	ASSERT(0);
-	return "";
-}
-
-//-----------------------------------------------------------------------------
-
-} // namespace jl
-
-//-----------------------------------------------------------------------------
-
-ENUM_NAME_GETTER(jl::TextureFilteringMode)
-
-//-----------------------------------------------------------------------------
+	LinearMipmapLinear
+)
--- src/utils/include/utils/EnumUtils.hpp
+++ src/utils/include/utils/EnumUtils.hpp
@@ -1,16 +1,39 @@
 #pragma once
 
-#include "utils/exceptions/ParseErrorException.hpp"
+#include "utils/Assert.hpp"
 
 #include <fmt/format.h>
-
 #include <string_view>
+#include <array>
 
 //-----------------------------------------------------------------------------
 
 namespace details {
+
 constexpr int k_minEnumValue{ 0 };
+
+template<typename T>
+constexpr auto getEnumNamesFromValues(std::string_view _string, std::string_view _delimiter)
+{
+	std::array<std::string_view, static_cast<int>(T::Count)> result;
+
+	int tokenStart = 0;
+	for (int i{ 0 }; i < static_cast<int>(T::Count); ++i)
+	{
+		int tokenEnd = _string.find(_delimiter, tokenStart);
+		if (tokenEnd == std::string_view::npos)
+		{
+			tokenEnd = _string.size();
+		}
+
+		result[i] = _string.substr(tokenStart, tokenEnd);
+		tokenStart = tokenEnd + _delimiter.size();
+	}
+
+	return result;
 }
+
+} // namespace details
 
 //-----------------------------------------------------------------------------
 
@@ -18,27 +41,6 @@ template<typename T>
 constexpr std::enable_if_t<std::is_enum_v<T>, bool> isEnumValueValid(T _value) noexcept
 {
 	return static_cast<int>(_value) >= details::k_minEnumValue && _value < T::Count;
-}
-
-//-----------------------------------------------------------------------------
-
-template<typename T>
-T fromString(std::string_view _str)
-{
-	constexpr int count{ static_cast<int>(T::Count) };
-	for (int i{ details::k_minEnumValue }; i < count; ++i)
-	{
-		const T current{ static_cast<T>(i) };
-		if (toString(current) == _str)
-		{
-			return current;
-		}
-	}
-
-	return T::Count;
-	//throw ParseErrorException(
-	//	fmt::format("Invalid enumerator: {}, while parsing enum {}", _str.data(), getEnumName<T>().data())
-	//);
 }
 
 //-----------------------------------------------------------------------------
@@ -58,6 +60,51 @@ constexpr std::string_view getEnumName<_enum>() noexcept	\
 
 //-----------------------------------------------------------------------------
 
-#define ENUM_TO_STRING_CASE(_enum, _case) case _enum::_case: return #_case
+#define NAMESPACE_ENUM_WITH_UTILITY(_namespace, _enumName, ...)									\
+namespace _namespace {																			\
+enum class _enumName																			\
+{																								\
+	__VA_ARGS__,																				\
+	Count																						\
+};																								\
+}																								\
+																								\
+namespace details {																				\
+constexpr std::string_view __##_enumName##_initData = #__VA_ARGS__;								\
+}																								\
+																								\
+constexpr std::string_view toString(_enumName _value)											\
+{																								\
+	using EnumValuesNames = std::array<std::string_view, static_cast<int>(_enumName::Count)>;	\
+	constexpr EnumValuesNames k_enumValuesNames =												\
+		details::getEnumNamesFromValues<_enumName>(details::__##_enumName##_initData, ", ");	\
+																								\
+	if (isEnumValueValid(_value))																\
+	{																							\
+		return k_enumValuesNames[static_cast<int>(_value)];										\
+	}																							\
+	ASSERT(0);																					\
+	return "";																					\
+}																								\
+																								\
+ENUM_NAME_GETTER(_enumName)
+
+//-----------------------------------------------------------------------------
+
+template<typename T>
+constexpr std::enable_if_t<std::is_enum_v<T>, T> fromString(std::string_view _str)
+{
+	constexpr int count{ static_cast<int>(T::Count) };
+	for (int i{ details::k_minEnumValue }; i < count; ++i)
+	{
+		const T current{ static_cast<T>(i) };
+		if (toString(current) == _str)
+		{
+			return current;
+		}
+	}
+
+	return T::Count;
+}
 
 //-----------------------------------------------------------------------------
