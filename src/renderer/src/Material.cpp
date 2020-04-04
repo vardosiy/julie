7288b2803239c33d325590b956e518b04522ee7f
Vladislav Smirnov <bf3le.2012@gmail.com>
2020-04-04 12:54:37 +0300

    - moved PropertyBinder into separate source file

--- src/renderer/src/Material.cpp
+++ src/renderer/src/Material.cpp
@@ -1,46 +1,10 @@
 #include "renderer/Material.hpp"
 #include "renderer/Shader.hpp"
-#include "renderer/Texture.hpp"
-#include "renderer/CubeTexture.hpp"
+#include "PropertyBinder.hpp"
 
 //-----------------------------------------------------------------------------
 
 namespace jl {
-
-//-----------------------------------------------------------------------------
-
-struct PropertyBinder
-{
-	PropertyBinder(const jl::Shader& _shader, const std::string& _uniformName, jl::s16 _textureSlot) noexcept
-		: m_shader(_shader)
-		, m_uniformName(_uniformName)
-		, m_textureSlot(_textureSlot)
-	{
-	}
-
-	void operator() (const jl::Texture* _texture) const noexcept
-	{
-		_texture->bind(m_textureSlot);
-		m_shader.setUniform(m_uniformName, m_textureSlot);
-	}
-
-	void operator() (const jl::CubeTexture* _texture) const noexcept
-	{
-		_texture->bind(m_textureSlot);
-		m_shader.setUniform(m_uniformName, m_textureSlot);
-	}
-
-	template<typename T>
-	void operator() (const T& _value) const noexcept
-	{
-		m_shader.setUniform(m_uniformName, _value);
-	}
-
-private:
-	const jl::Shader& m_shader;
-	const std::string& m_uniformName;
-	jl::s16 m_textureSlot;
-};
 
 //-----------------------------------------------------------------------------
 
---
+++ src/renderer/src/PropertyBinder.hpp
@@ -0,0 +1,50 @@
+#include "renderer/Types.hpp"
+#include "renderer/Shader.hpp"
+#include "renderer/Texture.hpp"
+#include "renderer/CubeTexture.hpp"
+
+//-----------------------------------------------------------------------------
+
+namespace jl {
+
+//-----------------------------------------------------------------------------
+
+class PropertyBinder
+{
+public:
+	PropertyBinder(const Shader& _shader, const std::string& _uniformName, s16 _textureSlot) noexcept
+		: m_shader(_shader)
+		, m_uniformName(_uniformName)
+		, m_textureSlot(_textureSlot)
+	{
+	}
+
+	void operator() (const Texture* _texture) const noexcept
+	{
+		_texture->bind(m_textureSlot);
+		m_shader.setUniform(m_uniformName, m_textureSlot);
+	}
+
+	void operator() (const CubeTexture* _texture) const noexcept
+	{
+		_texture->bind(m_textureSlot);
+		m_shader.setUniform(m_uniformName, m_textureSlot);
+	}
+
+	template<typename T>
+	void operator() (const T& _value) const noexcept
+	{
+		m_shader.setUniform(m_uniformName, _value);
+	}
+
+private:
+	const Shader& m_shader;
+	const std::string& m_uniformName;
+	s16 m_textureSlot;
+};
+
+//-----------------------------------------------------------------------------
+
+} // namespace jl
+
+//-----------------------------------------------------------------------------
