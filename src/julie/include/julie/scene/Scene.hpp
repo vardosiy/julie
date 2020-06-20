945a30034da693e7173c4cdb36ba60f3b328fb76
Vladislav Smirnov <bf3le.2012@gmail.com>
2020-06-20 14:59:29 +0300

    * missing file for previous commit

--- src/julie/include/julie/scene/Scene.hpp
+++ src/julie/include/julie/scene/Scene.hpp
@@ -1,13 +1,13 @@
 #pragma once
 
 #include "julie/core/Types.hpp"
+#include "julie/scene/Object.hpp"
 #include "julie/scene/FogData.hpp"
 #include "julie/scene/lights/LightsHolder.hpp"
 
 #include <boost/optional.hpp>
-#include <glm/glm.hpp>
+#include <boost/noncopyable.hpp>
 
-#include <functional>
 #include <memory>
 #include <vector>
 
@@ -17,22 +17,17 @@ namespace jl {
 
 //-----------------------------------------------------------------------------
 
-class Object;
 class Camera;
-class Model;
 
-class Scene
+class Scene : boost::noncopyable
 {
 //-----------------------------------------------------------------------------
 public:
 	using ObjectPtr = std::unique_ptr<Object>;
 
-	Scene();
-	~Scene();
-
+//-----------------------------------------------------------------------------
 	void render(const Camera& _cam) const;
 
-//-----------------------------------------------------------------------------
 	const FogData* getFogData() const noexcept;
 	void setFogData(const FogData& _data) noexcept;
 
@@ -43,21 +38,17 @@ public:
 	void addObject(ObjectPtr&& _obj) noexcept;
 	u32 getObjectsCount() const noexcept;
 
-	Object* getObject(u32 _idx) noexcept;
-	const Object* getObject(u32 _idx) const noexcept;
+	Object& getObject(u32 _idx);
+	const Object& getObject(u32 _idx) const;
 
-	void removeObject(u32 _idx) noexcept;
-	void removeObject(const Object& _obj) noexcept;
+	ObjectPtr eraseObject(u32 _idx) noexcept;
+	ObjectPtr eraseObject(const Object& _obj) noexcept;
 
 //-----------------------------------------------------------------------------
 private:
-	void drawModel(const Model& _model, const Camera& _cam, const glm::mat4& _worldMat) const noexcept;
-
-//-----------------------------------------------------------------------------
 	std::vector<ObjectPtr> m_objects;
 
 	LightsHolder m_lightsHolder;
-
 	boost::optional<FogData> m_fogData;
 };
 
